3cb6524b906c9ab8bd53ccc94e6fa5cc5def17df
Ibtid <ibtid011@example.com>
2021-06-27 20:13:34 +0600

    Database connected

--- Lab_2_180042143(Authentication and Authorization)/index.js
+++ Lab_2_180042143(Authentication and Authorization)/index.js
@@ -1,5 +1,21 @@
 const app = require('./app.js');
 require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Database Connected');
+  })
+  .catch((err) => {
+    if (err) {
+      console.log('Unable to connect to Mongoose');
+    }
+  });
 
 const PORT = process.env.PORT;
 
