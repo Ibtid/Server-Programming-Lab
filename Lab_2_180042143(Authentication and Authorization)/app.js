57570a87383382db6308a9c96ac21cdb1889ccc7
Ibtid <ibtid011@example.com>
2021-06-28 00:21:55 +0600

    styled

--- Lab_2_180042143(Authentication and Authorization)/app.js
+++ Lab_2_180042143(Authentication and Authorization)/app.js
@@ -2,11 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const userRoutes = require('./routes/userRoutes.routes');
 const bodyParser = require('body-parser');
-
 const app = express();
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 app.use(userRoutes);
 
