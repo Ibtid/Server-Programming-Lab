75f04a923badcce4d89eb992aef704937febd50c
Ibtid <ibtid011@example.com>
2021-06-28 16:32:48 +0600

    can register into database

--- Lab_2_180042143(Authentication and Authorization)/controllers/userControllers.controller.js
+++ Lab_2_180042143(Authentication and Authorization)/controllers/userControllers.controller.js
@@ -1,4 +1,6 @@
 const User = require('../models/userModel.model');
+const bcrypt = require('bcryptjs');
+const alert = require('alert');
 
 const getLoginPage = (req, res) => {
   res.sendFile('login.html', { root: './views/pages/examples' });
@@ -8,14 +10,49 @@ const getRegisterPage = (req, res) => {
   res.sendFile('register.html', { root: './views/pages/examples' });
 };
 
-const postRegister = (req, res) => {
+const postRegister = async (req, res) => {
   const name = req.body.name;
   const email = req.body.email;
   const password = req.body.password;
-  if (password.length < 6) {
-    return res.json({ message: 'Resgistration failed' });
+  const retype = req.body.retype;
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (user) {
+      alert('There is already an account under that email.');
+      //res.redirect('/login');
+      res.json({ message: 'failed' });
+    } else if (password.length < 6) {
+      alert('Password must be at least 6 characters');
+      //res.redirect('/register');
+      res.json({ message: 'failed' });
+    } else if (password !== retype) {
+      alert('Please enter the same password twice.');
+      //res.redirect('/register');
+      res.json({ message: 'failed' });
+    } else if (!name || !email) {
+      alert('Please fill the form.');
+      //res.redirect('/register');
+      res.json({ message: 'failed' });
+    } else {
+      const salt = await bcrypt.genSaltSync(10);
+      const passwordHash = await bcrypt.hash(password, salt);
+      const createUser = new User({
+        name,
+        email,
+        passwordHash,
+      });
+      await createUser.save();
+      alert('Successfully created');
+      //res.redirect('/login');
+      res.json({ message: 'success', createUser });
+    }
+  } catch (error) {
+    console.error(error);
+    alert('Something went wrong');
+    res.redirect('/register');
   }
-  res.json({ message: 'registration success' });
 };
 
 module.exports = { getLoginPage, getRegisterPage, postRegister };
